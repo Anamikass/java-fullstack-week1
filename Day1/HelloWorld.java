873e4901f2beb5784e7eda3738d8b38db332668b
shefali <56190884+Anamikass@users.noreply.github.com>
2025-06-28 02:45:19 -0700

    Create HelloWorld.java

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Backend!");
+        System.out.println("Today is myfirst day of Java!");
+        System.out.println(100+50); //maths
+    }
+}
