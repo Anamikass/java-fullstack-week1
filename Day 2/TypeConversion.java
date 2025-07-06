63a75d2b71ee2b21d949cec9b6c692960fb4ef7c
shefali <56190884+Anamikass@users.noreply.github.com>
2025-07-06 23:48:42 +0530

    TypeConversion.java

---
+++ Day 2/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        double marks = 89.7;
+        int roundedMarks = (int) marks;
+
+        System.out.println("Original: " + marks);
+        System.out.println("Rounded: " + roundedMarks);
+    }
+}
