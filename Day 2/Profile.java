6274bd30f7759ae815f26808998263de5c7bde53
shefali <56190884+Anamikass@users.noreply.github.com>
2025-07-06 23:46:51 +0530

    Profile.java

---
+++ Day 2/Profile.java
@@ -0,0 +1,13 @@
+public class Profile {
+    public static void main(String[] args) {
+        String name = "Anamika";
+        int age = 26;
+        double salary = 50000.75;
+        boolean isWorking = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Working? " + isWorking);
+    }
+}
